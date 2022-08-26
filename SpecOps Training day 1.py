44b6c2e6bbe8e950bbeece940d56e190ff689861
avetkost <kostandyan0298@gmail.com>
2022-08-27 00:42:56 +0400

    add task # 4 #5 #6 #7

--- SpecOps Training day 1.py
+++ SpecOps Training day 1.py
@@ -23,7 +23,7 @@ with open("File.txt", "r") as text_file:
             line = line.title()
             new_file.write(line)
 
-"""Task #3 Օգտագործելով աղյուսակ (dictionary) հաշվել զանգվածում բոլոր թվերի կրկնությունների քանակը։"""
+                    """Task #3 Օգտագործելով աղյուսակ (dictionary) հաշվել զանգվածում բոլոր թվերի կրկնությունների քանակը։"""
 
 my_array = [1, 2, 4, 5, 3, 4, 1, 2, 1, 2, 1, 3, 4, 5, 7, 8, 9]
 empty_array = {}
