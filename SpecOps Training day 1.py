c49e78611dae9081de0c9fac6cd46dc051026b98
avetkost <kostandyan0298@gmail.com>
2022-08-27 00:42:14 +0400

    add task # 4 #5 #6 #7

--- SpecOps Training day 1.py
+++ SpecOps Training day 1.py
@@ -1,4 +1,4 @@
-"""Task #1  Տրված ֆայլում պարունակվում են թվեր։ Իրականացնել ծրագիր, որը հաշվում և վերադարձնում է ֆայլում պարունակվող թվերի գումարը։"""
+                    """Task #1  Տրված ֆայլում պարունակվում են թվեր։ Իրականացնել ծրագիր, որը հաշվում և վերադարձնում է ֆայլում պարունակվող թվերի գումարը։"""
 
 sum = 0
 given_file = open(
@@ -14,8 +14,8 @@ print(f"sum is {sum}")
 
 given_file.close()
 
-"""Task #2 Տրված ֆայլում պարունակվում է տեքստ։ Իրականացնել ծրագիր, որը ֆայլի մեջ պարունակվող տեքստի բոլոր
-    բառերի առաջին տառերը դարձնում է մեծատառ և ձևափոխված ամբողջ տեքստը պահպանում մեկ այլ ֆայլում։"""
+                    """Task #2 Տրված ֆայլում պարունակվում է տեքստ։ Իրականացնել ծրագիր, որը ֆայլի մեջ պարունակվող տեքստի բոլոր
+                    բառերի առաջին տառերը դարձնում է մեծատառ և ձևափոխված ամբողջ տեքստը պահպանում մեկ այլ ֆայլում։"""
 
 with open("File.txt", "r") as text_file:
     with open("Cap_letter", "w") as new_file:
@@ -34,3 +34,57 @@ for num in my_array:
     else:
         empty_array[num] += 1
 print(empty_array)
+
+
+                    """Task #4 Տեքստային ֆայլի համար իրականացնել ծրագիր, որը կհաշվի ֆայլում հանդիպող սիմվոլների քանակը:"""
+
+sum = 0
+with open("File.txt", "r") as file:
+    for char in file.read():
+        if char != " " and char != "\n":
+            sum += 1
+print(sum)
+
+                    """Task #5 Իրականացնել ֆունկցիա, որը կհեռացնի ստացված տողի (string) ամեն երրորդ սիմվոլը։"""
+
+
+str_example = "123123123123123123123123"
+
+
+def del_third_sym(string, index):
+    new_string = ""
+    for i in range(0, len(string), index):
+        new_string += string[i : i + index - 1]
+    return new_string
+
+
+print(f"Input: {str_example}")
+print(f"Output: {del_third_sym(str_example, 3)}")
+
+                    """Task #6 Իրականացնել ծրագիր, որը կհաշվի թե յուրաքանչյուր բառ քանի անգամ է հանդիպում տեքստային ֆայլում։"""
+
+counter = {}
+with open(
+    "Lyric.txt",
+    "r",
+) as text:
+    for line in text:
+        for word in line.split():
+            if counter.get(word):
+                counter[word] += 1
+            else:
+                counter[word] = 1
+print(counter)
+
+
+                    """Task #7 Գրել ծրագիր, որը կհաշվի տրված զանգվածի արժեքների քառակուսիները և կպահի դրանք նոր զանգվածում՝ սորտավորված ձևով։"""
+
+massive_2 = [1, 2, 4, 5, 3, 4, 1, 2, 1, 2, 1, 3, 4, 5, 7, 8, 9]
+empety = []
+for x in massive_2:
+    empety.append(x**2)
+empety.sort()
+
+print(empety)
+
+
